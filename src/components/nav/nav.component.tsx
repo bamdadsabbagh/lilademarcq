f61c6b9cf486ded192fe76191d9113e12ad3121e
Bamdad Sabbagh <hi@bamdad.fr>
2022-04-17 18:30:27 +0200

    feat(Nav): Set fixed size for correct border calculations + Set dynamic active colors

--- src/components/nav/nav.component.tsx
+++ src/components/nav/nav.component.tsx
@@ -1,40 +1,53 @@
-import React, {ReactElement} from 'react';
+import React, {ReactElement, useState} from 'react';
+import {useRouter} from 'next/router';
 import {Nav, StyledList} from './nav.styles';
-import {MenuComponent} from './components/menu/menu.component';
+import {CellComponent} from './components/cell/cell.component';
 
 export default function NavComponent(): ReactElement {
+  const router = useRouter();
+
+  const [routes] = useState([
+    {text: 'home', href: '/'},
+    {text: 'à propos', href: '/a-propos'},
+    {
+      text: 'objets',
+      href: '/objets',
+      items: [
+        {text: 'empty-0'},
+        {text: 'Indlu', href: '/objets/indlu'},
+        {text: 'Isiqu', href: '/objets/isiqu'},
+        {text: 'Isihla', href: '/objets/isihla'},
+        {text: 'empty-1'},
+        {text: 'Sa-Poro', href: '/objets/sa-poro'},
+        {text: 'empty-2'},
+        {text: 'Usha papier', href: '/objets/usha-papier'},
+        {text: 'Usha acier', href: '/objets/usha-acier'},
+        {text: 'empty-3'},
+      ],
+    },
+    {text: 'poésie', href: '/poesie'},
+    {text: 'événements', href: '/evenements'},
+    {text: 'presse', href: '/presse'},
+    {text: 'livre d\'or', href: '/livre-d-or'},
+  ]);
+
   return (
     <Nav>
       <StyledList>
-        <MenuComponent k={0} main={{text: 'home', href: '/'}} noLeft active />
-        <MenuComponent
-          k={1}
-          main={{text: 'à propos', href: '/a-propos'}}
-          noLeft
-        />
-        <MenuComponent
-          k={2}
-          main={{text: 'objets', href: '/objets'}}
-          items={[
-            {text: 'empty-0'},
-            {text: 'Indlu', href: '/objets/indlu'},
-            {text: 'Isiqu', href: '/objets/isiqu'},
-            {text: 'Isihla', href: '/objets/isihla'},
-            {text: 'empty-1'},
-            {text: 'Sa-Poro', href: '/objets/sa-poro'},
-            {text: 'empty-2'},
-            {text: 'Usha papier', href: '/objets/usha-papier'},
-            {text: 'Usha acier', href: '/objets/usha-acier'},
-            {text: 'empty-3'},
-          ]}
-        />
-        <MenuComponent k={3} main={{text: 'poésie', href: '/poesie'}} />
-        <MenuComponent k={4} main={{text: 'événements', href: '/evenements'}} />
-        <MenuComponent k={5} main={{text: 'presse', href: '/presse'}} />
-        <MenuComponent
-          k={6} main={{text: 'livre d\'or', href: '/livre-d-or'}}
-          noRight
-        />
+        {routes.map((route, index) => {
+          const {text, href, items} = route;
+          return (
+            <CellComponent
+              key={text}
+              k={index}
+              primary={{text, href}}
+              noLeft={index === 0}
+              noRight={index === routes.length - 1}
+              active={router.pathname === href}
+              items={items || []}
+            />
+          );
+        })}
       </StyledList>
     </Nav>
   );
