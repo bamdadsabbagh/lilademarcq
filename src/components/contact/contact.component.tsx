d0bebada6d5c52d5fe71914b00c094b3b7687fd4
Bamdad Sabbagh <hi@bamdad.fr>
2022-04-18 23:51:50 +0200

    fix(ContactComponent): Prevent y overflow

--- src/components/contact/contact.component.tsx
+++ src/components/contact/contact.component.tsx
@@ -1,6 +1,6 @@
 import React, {ReactElement} from 'react';
 import ContactImage from '../../../public/images/contact.jpg';
-import {Container, Image, TextContainer} from './contact.styles';
+import {Container, Image, TextContainer, Wrapper} from './contact.styles';
 import {MarkdownComponent} from '../markdown/markdown.component';
 
 export interface ContactComponentProps {
@@ -9,18 +9,20 @@ export interface ContactComponentProps {
 
 export function ContactComponent({content}: ContactComponentProps): ReactElement {
   return (
-    <Container>
-      <Image
-        alt="contact"
-        src={ContactImage}
-        placeholder="blur"
-        objectFit="contain"
-        width={640 * 0.8}
-        height={480 * 0.8}
-      />
-      <TextContainer>
-        <MarkdownComponent content={content} />
-      </TextContainer>
-    </Container>
+    <Wrapper>
+      <Container>
+        <Image
+          alt="contact"
+          src={ContactImage}
+          placeholder="blur"
+          objectFit="contain"
+          width={640 * 0.8}
+          height={480 * 0.8}
+        />
+        <TextContainer>
+          <MarkdownComponent content={content} />
+        </TextContainer>
+      </Container>
+    </Wrapper>
   );
 }
