d5713eb6ef0a6fd152341bd74cc25b6247fff286
Bamdad Sabbagh <hi@bamdad.fr>
2022-04-20 18:35:21 +0200

    fix(SectionTitle): Remove media queries for now

--- src/components/section-title/section-title.styles.ts
+++ src/components/section-title/section-title.styles.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import {fontFarmhouse} from '../../app/styles/fonts';
 import {AlignKeys} from './section-title.component';
-import {mediaQueries} from '../../app/styles/breakpoints';
 
 interface TitleProps {
   align: AlignKeys;
@@ -18,28 +17,4 @@ export const Title = styled.h2<TitleProps>`
   font-size: 4em;
 
   text-align: ${({align}) => align};
-
-  ${mediaQueries.above.mobile} {
-    font-size: 2.5em;
-  }
-
-  ${mediaQueries.above.tablet} {
-    font-size: 2.8em;
-  }
-
-  ${mediaQueries.above.desktop} {
-    font-size: 2.8em;
-  }
-
-  ${mediaQueries.above.widescreen} {
-    font-size: 2.8em;
-  }
-
-  ${mediaQueries.above.fullhd} {
-    font-size: 3em;
-  }
-
-  ${mediaQueries.above.qhd} {
-    font-size: 4em;
-  }
 `;
