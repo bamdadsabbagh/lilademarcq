b4ada47d39133fdec9648122f9d5db5032385377
Bamdad Sabbagh <hi@bamdad.fr>
2022-04-29 16:03:19 +0200

    perf: Make form post request on server side + Add utility function to get API endpoints

--- src/app/hooks/use-form-fetch.ts
+++ src/app/hooks/use-form-fetch.ts
@@ -1,13 +1,14 @@
 import {useEffect} from 'react';
 import {useAtom} from 'jotai';
 import {setFormAtom} from '../../atoms/form.atom';
+import {getApiEndpoint} from '../../utils/get-api-endpoint';
 
 export function useFormFetch(): void {
   const [, setForm] = useAtom(setFormAtom);
 
   useEffect(() => {
     (async () => {
-      const form = await fetch(`${window.location.origin}/api/form`);
+      const form = await fetch(getApiEndpoint('form'));
       const json = await form.json();
       setForm(json);
     })();
