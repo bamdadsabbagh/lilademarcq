4694aaeded030c60d73e29ea51439c5dc6153204
Bamdad Sabbagh <hi@bamdad.fr>
2022-05-03 09:56:26 +0200

    fix(Gallery): Add better badge styling

--- src/components/carousel/components/gallery/gallery.component.styles.ts
+++ src/components/carousel/components/gallery/gallery.component.styles.ts
@@ -1,15 +1,22 @@
 import styled from 'styled-components';
 import {MAX_WIDTH} from '../../../../constants';
+import {mediaQueries} from '../../../../app/styles/breakpoints';
 
 export const BadgeContainer = styled.div`
   position: absolute;
   display: flex;
   justify-content: flex-end;
 
-  width: min(90%, ${MAX_WIDTH});
-
-  top: -60px;
-
   pointer-events: none;
   user-select: none;
+
+  width: 125%;
+  top: -55px;
+  transform: scale(0.5);
+
+  ${mediaQueries.above.mobile} {
+    width: min(90%, ${MAX_WIDTH});
+    top: -60px;
+    transform: scale(1);
+  }
 `;
