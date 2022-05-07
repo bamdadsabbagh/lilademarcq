7b04e7e3028494434d2dbf940b9020da634357da
Bamdad Sabbagh <hi@bamdad.fr>
2022-05-07 11:54:49 +0200

    feat(About): Add mobile view to AboutLayout

---
+++ src/layouts/about/about.layout.styles.ts
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import {fontSpectral} from '../../app/styles/fonts';
+import {mediaQueries} from '../../app/styles/breakpoints';
+
+export const Container = styled.div`
+  display: grid;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 3em;
+  grid-template-rows: repeat(2, auto);
+
+  ${mediaQueries.above.tablet} {
+    grid-template-rows: none;
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
+
+export const ImageContainer = styled.div`
+  display: block;
+  justify-self: flex-end;
+  width: 100%;
+
+  ${mediaQueries.above.tablet} {
+    width: 90%;
+  }
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  height: 100%;
+
+  ${fontSpectral};
+  font-weight: 200;
+  font-size: 1.2em;
+
+  transform: translateY(-2px);
+`;
+
+export const TextWrapper = styled.div`
+  h2 {
+    font-size: 1.4em;
+    margin-bottom: 1.5em;
+  }
+
+  ${mediaQueries.above.tablet} {
+    h2 {
+      margin-bottom: 1em;
+      margin-left: -0.7em;
+    }
+
+    p {
+      margin-left: 0.7em;
+    }
+  }
+
+  ${mediaQueries.above.desktop} {
+    h2 {
+      margin-bottom: 1.5em;
+      margin-left: -1em;
+    }
+
+    p {
+      margin-left: 1em;
+    }
+  }
+`;
---
+++ src/layouts/about/about.layout.tsx
@@ -0,0 +1,55 @@
+import React, {ReactElement} from 'react';
+import Image from 'next/image';
+import {documentToReactComponents} from '@contentful/rich-text-react-renderer';
+import {SectionComponent} from '../../components/section/section.component';
+import {
+  Container,
+  ImageContainer,
+  TextContainer,
+  TextWrapper,
+} from './about.layout.styles';
+import {DefaultLayout} from '../default/default.layout';
+import {LDSection} from '../../utils/fetch-section';
+
+interface AboutLayoutProps {
+  aboutSection: LDSection;
+}
+
+export function AboutLayout({
+  aboutSection,
+}: AboutLayoutProps): ReactElement {
+  return (
+    <>
+      <DefaultLayout>
+
+        <SectionComponent isSmallTop>
+
+          <Container>
+
+            <ImageContainer>
+              <Image
+                src={aboutSection.image.url}
+                alt="portrait"
+                layout="responsive"
+                width="100%"
+                height="100%"
+                placeholder="blur"
+                blurDataURL={aboutSection.image.base64}
+              />
+            </ImageContainer>
+
+            <TextContainer>
+              <TextWrapper>
+                <h2>{aboutSection.title}</h2>
+                {documentToReactComponents(aboutSection.body.json)}
+              </TextWrapper>
+            </TextContainer>
+
+          </Container>
+
+        </SectionComponent>
+
+      </DefaultLayout>
+    </>
+  );
+}
